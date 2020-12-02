595203dd2eb6de1c4f8e53ca22053d09221ec8dd
Rex Schilasky <49162693+rex-schilasky@users.noreply.github.com>
2020-12-02 09:49:59 +0100

    cleanup CMakeLists.txt to better group source files (#12)
    removed all doubled copyright headers

--- rmw_ecal_dynamic_cpp/src/internal/rosidl_generator_c_pkg_adapter.hpp
+++ rmw_ecal_dynamic_cpp/src/internal/rosidl_generator_c_pkg_adapter.hpp
@@ -12,25 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* ========================= RMW eCAL LICENSE =================================
-*
-* Copyright (C) 2019 - 2020 Continental Corporation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* ========================= RMW eCAL LICENSE =================================
-*/
-
 //From Foxy distro rosidl_generator_c has been renamed to rosidl_generator_c,
 //purpose of this header file is to alias old C type names to new ones, that way the
 //rest of the code doesn't have to care about old package name regardless of distro.
