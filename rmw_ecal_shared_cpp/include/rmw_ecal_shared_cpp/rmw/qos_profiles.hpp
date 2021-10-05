3d4dd53fb5cf16844c3eacb059765cf7ce28e736
Aleksandar Brakmić <13668697+brakmic-aleksandar@users.noreply.github.com>
2021-10-05 10:20:48 +0200

    Fixed linking error on Windows

--- rmw_ecal_shared_cpp/include/rmw_ecal_shared_cpp/rmw/qos_profiles.hpp
+++ rmw_ecal_shared_cpp/include/rmw_ecal_shared_cpp/rmw/qos_profiles.hpp
@@ -23,6 +23,7 @@ namespace eCAL
 {
   namespace rmw
   {
+    RMW_PROTOBUF_SHARED_CPP_PUBLIC
     rmw_ret_t rmw_qos_profile_check_compatible(const rmw_qos_profile_t publisher_profile,
                                                const rmw_qos_profile_t subscription_profile,
                                                rmw_qos_compatibility_type_t *compatibility,
